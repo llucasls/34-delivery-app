a3178f546788defd6aac70ff05ff138b3be7f2c9
Filipe <filipecandido123@hotmail.com>
2022-04-28 13:25:48 -0300

    [fix]: ajustado o nome da rota de "cadastro" para "register"

--- front-end/src/router/welcome.route.js
+++ front-end/src/router/welcome.route.js
@@ -8,7 +8,7 @@ import Register from '../pages/Register/Regiser';
 const WelcomeRoute = () => (
   <Switch>
     <Route path="/" exact element={ <Login /> } />
-    <Route path="/cadastro" exact element={ <Register /> } />
+    <Route path="/register" exact element={ <Register /> } />
   </Switch>
 );
 
