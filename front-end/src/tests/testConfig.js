f80d08032a6ec8f05d3e5c22f7fb1b0c3ca68da7
Filipe <filipecandido123@hotmail.com>
2022-04-28 19:04:49 -0300

    [feat]: remove comentários do testconfig

--- front-end/src/tests/testConfig.js
+++ front-end/src/tests/testConfig.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// import { createMemoryHistory } from 'history';
 import { render as rtlRender } from '@testing-library/react';
-// import { configureStore } from '@reduxjs/toolkit';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-// import userReducer from '../store/slices/user';
 import defaultTheme from '../theme';
 import { store } from '../store';
 import '@testing-library/jest-dom';
 
-// const routeConfig = {}
-// const storeConfig = {}
-
-// export const getStore = () => {
-//   const store = configureStore({ reducer: { user: userReducer } });
-
-//   return store;
-// };
-
-// initialEntries = ['/'],
-// history = createMemoryHistory({ initialEntries }),
-
 function render(component) {
-  // const store = getStore();
-  // const route = routeConfigs.route || '/';
-  // const history = routeConfigs.history
-  // || createMemoryHistory({ initialEntries: [route] });
-
   return {
     ...rtlRender(
       <Provider store={ store }>
