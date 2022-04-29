2275dd6f57675184b427809235bcba5da9c87ae5
Filipe <filipecandido123@hotmail.com>
2022-04-28 23:00:25 -0300

    [style]: importação de estilo em register

--- front-end/src/components/Form/Button/Button.js
+++ front-end/src/components/Form/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './style.css';
 import { StyledViewButton, StyledButton } from './styles';
 
 const Button = ({ size, title, ...rest }) => (
