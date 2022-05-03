32bb816a4946f9dcc06f857d2f0604c035ee5c09
Filipe <filipecandido123@hotmail.com>
2022-05-03 18:31:53 -0300

    [feat]: add axios get products in page customer/products

--- front-end/src/pages/Products/Products.js
+++ front-end/src/pages/Products/Products.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import { StyledPage } from './styles';
 import ProductCard from '../../components/ProductCard/ProductCard';
+import { api } from '../../service/api';
+import { useAppDispatch } from '../../store';
+import { SET_PRODUCT } from '../../store/slices/products';
 
-const Products = () => (
-  <StyledPage>
-    <NavBar />
-    <ProductCard />
-  </StyledPage>
-);
+const Products = () => {
+  const dispatch = useAppDispatch();
+
+  const productsApi = useCallback(async () => {
+    try {
+      const { data } = await api.get('/products');
+      console.log(data);
+      dispatch(SET_PRODUCT({ products: data }));
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    productsApi();
+  }, [productsApi]);
+
+  return (
+    <StyledPage>
+      <NavBar />
+      <ProductCard />
+    </StyledPage>
+  );
+};
 
 export default Products;
