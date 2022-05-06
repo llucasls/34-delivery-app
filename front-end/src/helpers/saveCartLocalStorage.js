0018e1766c050dab0692b0f1a5c809779a2b1a63
Filipe <filipecandido123@hotmail.com>
2022-05-06 19:35:54 -0300

    [refactor]: Storage que adiciona bebidas no carrinho

--- front-end/src/helpers/saveCartLocalStorage.js
+++ front-end/src/helpers/saveCartLocalStorage.js
@@ -1,19 +1,40 @@
 export const cartTotal = () => {
   const cartProducts = (JSON.parse(localStorage.getItem('cart')) || []);
-  const totalItens = cartProducts
-    .reduce((prev, { total }) => prev + Number(total), 0);
-  return totalItens;
+  const result = cartProducts
+    .reduce((acc, curr) => acc + Number(curr.price) * curr.amount, 0);
+  return result;
 };
 
-const handleAddToCart = (product, total) => {
+const handleAddToCart = (product, updateCart) => {
+  const NOT_FOUND = -1;
+  product.amount = 1;
   const cart = JSON.parse(localStorage.getItem('cart'));
-  if (cart) {
-    const newCart = [...cart, { ...product, total }];
-    localStorage.setItem('cart', JSON.stringify(newCart));
+
+  if (cart !== null) {
+    const productIndex = cart
+      .findIndex((cartProduct) => cartProduct.id === product.id);
+
+    if (productIndex !== NOT_FOUND) {
+      cart[productIndex].amount += 1;
+      localStorage.setItem('cart', JSON.stringify([...cart]));
+    } else {
+      localStorage.setItem('cart', JSON.stringify([...cart, product]));
+    }
   } else {
-    localStorage.setItem('cart', JSON.stringify([{ ...product, total }]));
+    localStorage.setItem('cart', JSON.stringify([product]));
   }
+  updateCart();
 };
+//   if (cart) {
+//     const newCart = [{ ...product, amount }];
+
+//     localStorage.setItem('cart', JSON.stringify([...cart, ...newCart]));
+
+//     // localStorage.setItem('cart', JSON.stringify(cartProducts));
+//   } else {
+//     localStorage.setItem('cart', JSON.stringify([{ ...product, amount }]));
+//   }
+// };
 
 export const handleRemoveToCart = (product) => {
   const cart = JSON.parse(localStorage.getItem('cart'));
