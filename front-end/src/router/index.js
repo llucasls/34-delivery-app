b418fa9545234aa2a3873f6ab88bf63e55717e18
Filipe <filipecandido123@hotmail.com>
2022-05-18 21:41:32 -0300

    [refactor]: rotas de customer/orders

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -9,7 +9,8 @@ import SellerDetails from '../pages/SellerDetails/SellerDetails';
 import Products from '../pages/Products/Products';
 import ProductsCheckout from '../pages/ProductsCheckout/ProductsCheckout';
 import Admin from '../pages/Admin/Admin';
-import ProductsDetails from '../pages/ProductsDetails/ProductsDetails';
+import ProductsOrders from '../pages/ProductsOrders/ProductsOrders';
+import OrderDetails from '../pages/ProductsOrders/OrderDetails/OrderDetails';
 
 const MainRoute = () => (
   <BrowserRouter>
@@ -21,9 +22,9 @@ const MainRoute = () => (
       <Route path="/customer/products" element={ <Products /> } />
       <Route path="/seller/orders" exact element={ <SellerOrders /> } />
       <Route path="/seller/orders/:id" exact element={ <SellerDetails /> } />
-      <Route path="/customer/orders/:id" exact element={ <ProductsDetails /> } />
-      <Route path="/customer/orders/" exact element={ <ProductsDetails /> } />
-      <Route path="/customer/orders/:id" exact element={ <ProductsDetails /> } />
+      <Route path="/customer/orders/:id" exact element={ <OrderDetails /> } />
+      <Route path="/customer/orders/" exact element={ <ProductsOrders /> } />
+      <Route path="/customer/orders/:id" exact element={ <OrderDetails /> } />
       <Route path="/admin" exact element={ <Admin /> } />
     </Switch>
   </BrowserRouter>
