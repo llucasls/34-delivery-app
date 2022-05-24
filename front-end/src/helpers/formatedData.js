4bbcba0c243348f1473baa41c6b4ab2ebfea86af
Filipe <filipecandido123@hotmail.com>
2022-05-24 00:04:08 -0300

    [fix]: formatedData

--- front-end/src/helpers/formatedData.js
+++ front-end/src/helpers/formatedData.js
@@ -1,15 +1,16 @@
 const formatedData = (dataUnformated) => {
-  const data = new Date(dataUnformated);
+  const NUMBERTEEN = 10;
+  const date = new Date(dataUnformated).toISOString().slice(0, NUMBERTEEN).split('-')
+    .reverse()
+    .join('/');
 
-  const day = data.getDay() + 1;
-  const month = data.getMonth() + 1;
-  const yaer = data.getFullYear();
+  // const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  // date.toLocaleDateString('pt-BR', options);
+  const day = date.split('/')[0];
+  const month = date.split('/')[1];
+  const yaer = date.split('/')[2];
 
-  const NUMBERTEN = 10;
-
-  return `${day > NUMBERTEN ? day : `0${day}`}/
-    ${month > NUMBERTEN ? month : `0${month}`}/
-    ${yaer}`;
+  return `${(day)}/${(month)}/${yaer}`;
 };
 
 export default formatedData;
